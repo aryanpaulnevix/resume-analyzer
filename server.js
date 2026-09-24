a943436f058214e1c4294cebc7da9aef306ae0cd
Aryan Paul <aryanpaul.kolkata@gmail.com>
2026-09-24 19:39:27 +0530

    configure pdf parser for vercel

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
 const { Groq } = require("groq-sdk");
+require("pdf-parse/worker");
 const { PDFParse } = require("pdf-parse");
 
 const createResumePrompt = require("./prompts/resumePrompt");
